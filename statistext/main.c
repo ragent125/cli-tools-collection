a351be9c91cc85b8c2bdc2f680ec6bf3c5c85894
Rayan PC <elkarounirayan@outlook.com>
2025-11-12 22:51:04 +0100

    v0.1: gestion des arguments

--- statistext/main.c
+++ statistext/main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     printf("Statistext - Les mots qui comptent !\n");
-    printf("Version 0.1 - En développement\n\n");
+    printf("Version 0.1 - Initialisation\n\n");
     
-    // Vérifier qu'un fichier est fourni en argument
+    /*
+    - Un seul et unique fichier analysé
+    - Extension <.txt>
+    */
+
     if (argc != 2) {
         printf("Usage: %s <fichier.txt>\n", argv[0]);
         return 1;
     }
+
+    char *filename = argv[1];
+    size_t file_len = strlen(filename);
+
+    if (file_len < 4 || strcmp(filename + file_len - 4, ".txt") != 0) {
+        printf("Erreur: Le fichier doit avoir l'extension .txt\n");
+        return 1;
+    }
     
-    printf("Analyse du fichier : %s\n", argv[1]);
+    printf("Analyse du fichier : %s\n", filename);
     printf("(Fonctionnalité à implémenter)\n");
     
     return 0;
